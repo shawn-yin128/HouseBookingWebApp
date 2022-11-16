9b97a01339c25f07b43a8706b3cf73b5b9bd586b
shawn-yin128 <yxyoung128@gmail.com>
2022-11-16 02:27:39 -0500

    fit(repo): add user and authoriy repository

---
+++ backend/src/main/java/com/housebooking/backend/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.housebooking.backend.repository;
+
+import com.housebooking.backend.model.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
